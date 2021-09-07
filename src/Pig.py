3c015ac38cb57417ef4b327914b526ec9f575ac1
Anton Ekström <anton.e.ekstrom@gmail.com>
2021-09-07 16:23:35 +0200

    Further breaks down the code into smaller functions

--- src/Pig.py
+++ src/Pig.py
@@ -7,85 +7,91 @@ import random
 
 
 def run():
-    win_points = 20  # Points to win (decrease if testing)
+    # initialize game state
+    win_points = 20
     aborted = False
-    # Hard coded players, replace *last* of all with ... (see below)
-    # players = [Player(name='Olle'), Player(name='Fia')]
-    players = get_players()    # ... this (method to read in all players)
+    players = get_players()
+    current_player = get_random_player(players)
 
-    # prints name of players in the game
-    show_players_msg(players)
-
+    # show startup messages
+    players_msg(players)
     welcome_msg(win_points)
     status_msg(players)
 
-    # get a random player to start
-    current_player = get_random_player(players)
-
     # repeat the game loop until a player has won (or a player quits)
-    while not game_has_ended(players, win_points) and not aborted:
-        # player can choose to roll or hold (or quit the game)
+    while not game_should_end(players, win_points, aborted):
         player_choice = get_player_choice(current_player)
-
-        # handle player choice
-        # quit
-        if player_choice == "q":
-            aborted = True
-        # next / hold
-        elif player_choice == "n":
-            # if the player chooses to hold, update points and select the next player
-            hold_pts(current_player)
-            current_player = get_next_player(players, current_player)
-            status_msg(players)
-        # roll
-        elif player_choice == "r":
-            roll = roll_die()
-
-            # update player points
-            next_points = get_next_round_pts(current_player.roundPts, roll)
-            current_player.roundPts = next_points
-
-            round_msg(roll, current_player)
-
-            # if the player rolls a 1, select the next player
-            if next_points == 0:
-                current_player = get_next_player(players, current_player)
-                status_msg(players)
-        # incorrect choice
-        else:
-            show_choices_msg()
-            pass
+        current_player, aborted = handle_player_choice(players, current_player, player_choice)
 
     game_over_msg(get_winning_player(players), aborted)
 
 
 class Player:
     def __init__(self, name=''):
-        self.name = name  # default ''
-        self.totalPts = 0  # Total points for all rounds
-        self.roundPts = 0  # Points for a single round
+        self.name = name  # default
+        self.total_points = 0  # Total points for all rounds
+        self.round_points = 0  # Points for a single round
+
+    def hold_points(self):
+        self.total_points += self.round_points
+        self.round_points = 0
+
+    def update_round_points_with_roll(self, roll):
+        if roll == 1:
+            self.round_points = 0
+        else:
+            self.round_points += roll
 
 
 # ---- Game logic methods --------------
-def roll_die():
+def handle_player_choice(players, current_player, player_choice):
+    aborted = False
+    next_player = current_player
+
+    if player_choice == "q":  # quit
+        aborted = True
+    elif player_choice == "n":  # next / hold
+        next_player = player_choice_next(players, current_player)
+    elif player_choice == "r":  # roll
+        next_player = player_choice_roll(players, current_player)
+    else:  # incorrect choice
+        choices_msg()
+
+    return next_player, aborted
+
+
+def player_choice_roll(players, current_player):
+    roll = get_die_roll()
+    current_player.update_round_points_with_roll(roll)
+    round_msg(roll, current_player)
+    return determine_next_player_after_roll(players, current_player)
+
+
+def player_choice_next(players, current_player):
+    current_player.hold_points()
+    status_msg(players)
+    return get_next_player(players, current_player)
+
+
+def determine_next_player_after_roll(players, current_player):
+    if current_player.round_points == 0:
+        status_msg(players)
+        return get_next_player(players, current_player)
+    else:
+        return current_player
+
+
+def game_should_end(players, win_points, aborted):
+    return someone_has_won(players, win_points) or aborted
+
+
+def get_die_roll():
     return random.randint(1, 6)
 
 
-def get_next_round_pts(round_total, roll):
-    if roll == 1:
-        return 0
-    else:
-        return round_total + roll
-
-
-def hold_pts(player):
-    player.totalPts += player.roundPts
-    player.roundPts = 0
-
-
-def game_has_ended(players, win_points):
+def someone_has_won(players, win_points):
     for player in players:
-        if player.totalPts >= win_points:
+        if player.total_points >= win_points:
             return True
     return False
 
@@ -96,7 +102,7 @@ def get_next_player(players, current_player):
 
 
 def get_player_total(player):
-    return player.totalPts
+    return player.total_points
 
 
 def get_winning_player(players):
@@ -107,18 +113,18 @@ def get_winning_player(players):
 def welcome_msg(win_pts):
     print("Welcome to PIG!")
     print("First player to get " + str(win_pts) + " points will win!")
-    show_choices_msg()
+    choices_msg()
 
 
 def status_msg(the_players):
     print("Points: ")
     for player in the_players:
-        print("\t" + player.name + " = " + str(player.totalPts) + " ")
+        print("\t" + player.name + " = " + str(player.total_points) + " ")
 
 
 def round_msg(result, current_player):
     if result > 1:
-        print("Got " + str(result) + " running total are " + str(current_player.roundPts))
+        print("Got " + str(result) + " running total are " + str(current_player.round_points))
     else:
         print("Got 1 lost it all!")
 
@@ -128,14 +134,14 @@ def game_over_msg(player, is_aborted):
         print("Aborted")
     else:
         print("Game over! Winner is player " + player.name + " with "
-              + str(player.totalPts + player.roundPts) + " points")
+              + str(player.total_points + player.round_points) + " points")
 
 
-def show_choices_msg():
+def choices_msg():
     print("Commands are: r = roll , n = next, q = quit")
 
 
-def show_players_msg(players):
+def players_msg(players):
     print(f"Players are {[player.name for player in players]}")
 
 
@@ -167,39 +173,19 @@ def test():
     # This is hard coded test data
     # An array of (no name) Players (probably don't need any name to test)
     test_players = [Player(), Player(), Player()]
-    sample_rolls = [roll_die() for _ in range(1000)]
-    win_pts = 20
-
-    # everything should print True
+    sample_rolls = [get_die_roll() for _ in range(1000)]
 
     # only rolls numbers [1-6]
     print(set(range(1, 7)).issubset(sample_rolls) and set(sample_rolls).issubset(range(1, 7)))
 
-    print(get_next_round_pts(7, 1) == 0)
-    print(get_next_round_pts(7, 2) == 9)
-    print(get_next_round_pts(0, 1) == 0)
-    print(get_next_round_pts(0, 2) == 2)
-
-    p = test_players[0]
-    p.roundPts = 6
-    p.totalPts = 7
-    hold_pts(p)
-    print(p.roundPts == 0)
-    print(p.totalPts == 6+7)
-    p.totalPts = 0
-
-    print(not game_has_ended(test_players, win_pts))
-    p.totalPts = 19
-    print(not game_has_ended(test_players, win_pts))
-    p.totalPts = 20
-    print(game_has_ended(test_players, win_pts))
-
+    # should return the next player
     print(get_next_player(test_players, test_players[0]) == test_players[1])
     print(get_next_player(test_players, test_players[2]) == test_players[0])
 
-    test_players[0].totalPts = 1
+    # should return the player with the most points
+    test_players[0].total_points = 1
     print(get_winning_player(test_players) == test_players[0])
-    test_players[1].totalPts = 2
+    test_players[1].total_points = 2
     print(get_winning_player(test_players) == test_players[1])
 
     exit(0)
